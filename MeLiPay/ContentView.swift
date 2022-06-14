e146ef5412d0b6122c7fa5413853b0a295679d99
Joaquín Chávez <joaquin.chavez.b@gmail.com>
2022-06-14 12:06:15 -0400

    Initial Commit

---
+++ MeLiPay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeLiPay
+//
+//  Created by Joaquín Chávez on 14-06-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeLiPay/MeLiPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeLiPayApp.swift
+//  MeLiPay
+//
+//  Created by Joaquín Chávez on 14-06-22.
+//
+
+import SwiftUI
+
+@main
+struct MeLiPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
